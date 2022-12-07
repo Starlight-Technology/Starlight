3978ec6df0938e6058c79e84db0529adc152dddf
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2022-12-07 16:25:09 -0300

    summaries

--- Starlight.Standard/Helper.cs
+++ Starlight.Standard/Helper.cs
@@ -19,16 +19,33 @@ namespace Starlight.Standard
 
     public class Helper : IHelper
     {
-        public async Task<bool> ContainsSpecialCharacter(string value)
+        /// <summary>
+        /// Verify if a string contains special characters like "./;,#@$$%¨&*
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public Task<bool> ContainsSpecialCharacter(string value)
         {
             if (value.AsParallel().Any(c => !char.IsLetterOrDigit(c)))
-                return true;
+                return Task.FromResult(true);
 
-            return false;
+            return Task.FromResult(false);
         }
 
+        /// <summary>
+        /// Encrypt a password using SHA256
+        /// </summary>
+        /// <param name="pass"></param>
+        /// <returns></returns>
         public async Task<string> EncryptPass256(string pass) => await EncryptPass256(pass, null);
 
+        /// <summary>
+        /// Encrypt a password using SHA256 with a salt to avoid two or more passwords with the same hash
+        /// </summary>
+        /// <param name="pass"></param>
+        /// <param name="salt"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public async Task<string> EncryptPass256(string pass, string salt = null)
         {
             pass = pass.Trim();
@@ -52,6 +69,16 @@ namespace Starlight.Standard
             }
         }
 
+        /// <summary>
+        /// Encrypt a password using SHA256 with a salt to avoid two or more passwords with the same hash
+        /// </summary>
+        /// <param name="pass"></param>
+        /// <param name="salt"></param>
+        /// <param name="isCaseSensitive"></param>
+        /// <param name="maxLength"></param>
+        /// <param name="minLength"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public async Task<string> EncryptPass256(string pass, string salt = null, bool isCaseSensitive = false, int? maxLength = null, int? minLength = null)
         {
             if (!isCaseSensitive)
@@ -62,6 +89,11 @@ namespace Starlight.Standard
             throw new Exception("Password can't be null or empty.");
         }
 
+        /// <summary>
+        /// Verify if a string is null, empty or a white space
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public Task<bool> IsValidString(string value)
         {
             if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
@@ -70,6 +102,13 @@ namespace Starlight.Standard
             return Task.FromResult(true);
         }
 
+        /// <summary>
+        /// Verify if a string is null, empty or a white space, and have the max and min length especified
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="maxLength"></param>
+        /// <param name="minLength"></param>
+        /// <returns></returns>
         public Task<bool> IsValidString(string value, int? maxLength = null, int? minLength = null)
         {
             if (minLength.HasValue && value.Length >= minLength && maxLength.HasValue && value.Length <= maxLength)
@@ -82,6 +121,12 @@ namespace Starlight.Standard
             return Task.FromResult(false);
         }
 
+
+        /// <summary>
+        /// Verify if the string is an email
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public Task<bool> IsValidEmail(string value)
         {
             try
